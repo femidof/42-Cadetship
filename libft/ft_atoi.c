a3970e160d456d236d2687f7644d0116df47eaf9
Olorunfemi Durosinmi <oldurosi@e1z3r7p9.42.us.org>
2019-09-17 19:04:37 -0700

    Update of day 2

---
+++ libft/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oldurosi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/17 14:49:57 by oldurosi          #+#    #+#             */
+/*   Updated: 2019/09/17 15:48:54 by oldurosi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] && ((str[i] == '\t') ||
+				(str[i] == '\n') || (str[i] == '\r') ||
+				(str[i] == '\v') || (str[i] == '\f') ||
+				(str[i] == ' ')))
+		++i;
+	if (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = (result * 10) + (str[i] + '0');
+		i++;
+	}
+	return (sign * result);
+}
